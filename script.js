53cb239b0580f230860e52bb683b3e1988cbfe04
Roman Holubkov <sergeykatelyna@gmail.com>
2020-08-18 23:04:11 +0300

    Task #2

--- script.js
+++ script.js
@@ -12,3 +12,45 @@ addFullName(user);
 // user === { firstName: 'Ivan', lastName: 'Vasylchenko', fullName: 'Ivan Vasylchenko' }
 
 console.log(user);
+
+
+
+
+// Task #2
+const users = [
+    {
+      firstName: undefined,
+      lastName: 'Drogo',
+      fullName: 'Ivan Drogo',
+    },
+    {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      firstName: undefined,
+      lastName: 'Doe',
+      fullName: 'Doe Sam',
+    },
+    {
+      firstName: "Ken",
+      lastName: 'M',
+      fullName: 'Ken M',
+    },
+];
+
+function restoreNames(passedUsers) {
+    passedUsers.forEach(el => {
+        if (el.firstName === undefined) {
+            const tempFirstName = el.fullName.replace(el.lastName, "").trim();
+            el.firstName = tempFirstName;
+        }
+    });
+}
+
+
+restoreNames(users);
+// users === [..., {firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy'}, ...]
+
+console.log(users);
